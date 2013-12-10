cb6a2ae334253edd919649d103130d4c541a71db
Tareq A. Siraj <tareqsiraj@gmail.com>
2013-12-09 23:15:23 -0500

    Fixed project settings for plugin and added plugin load time code base
    - Fixed principal class name
    - Fixed deployment related settings

--- ClangFormatForXcode/ClangFormatPlugin.m
+++ ClangFormatForXcode/ClangFormatPlugin.m
@@ -10,4 +10,30 @@
 
 @implementation ClangFormatPlugin
 
+static ClangFormatPlugin *plugin = nil;
+
++ (void)pluginDidLoad:(NSBundle *)bundle {
+  NSBundle *app = [NSBundle mainBundle];
+  NSString *bundleIdentifier = [app bundleIdentifier];
+  // Load only for Xcode
+  if (![bundleIdentifier isEqualToString:@"com.apple.dt.Xcode"])
+    return;
+
+  NSLog(@"Clang Format plugin for Xcode loaded");
+
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{ plugin = [[self alloc] init]; });
+}
+
+- (id)init {
+  if (self = [super init]) {
+    [[NSAlert alertWithMessageText:@"Clang Format for Xcode loaded!"
+                     defaultButton:@"OK"
+                   alternateButton:nil
+                       otherButton:nil
+         informativeTextWithFormat:@""] runModal];
+  }
+  return self;
+}
+
 @end
