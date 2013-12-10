52137437e5a19e9b5379e01cd4440816521b2bf8
Tareq A. Siraj <tareqsiraj@gmail.com>
2013-12-09 22:52:38 -0500

    Initial commit for the plugin
    Note that this is only tested using Xcode 5.

---
+++ ClangFormatForXcode/ClangFormatPlugin.h
@@ -0,0 +1,13 @@
+//
+//  ClangFormatPlugin.h
+//  ClangFormatForXcode
+//
+//  Created by Tareq A. Siraj on 12/9/2013.
+//  Copyright (c) 2013 Tareq A. Siraj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClangFormatPlugin : NSObject
+
+@end
---
+++ ClangFormatForXcode/ClangFormatPlugin.m
@@ -0,0 +1,13 @@
+//
+//  ClangFormatPlugin.m
+//  ClangFormatForXcode
+//
+//  Created by Tareq A. Siraj on 12/9/2013.
+//  Copyright (c) 2013 Tareq A. Siraj. All rights reserved.
+//
+
+#import "ClangFormatPlugin.h"
+
+@implementation ClangFormatPlugin
+
+@end
